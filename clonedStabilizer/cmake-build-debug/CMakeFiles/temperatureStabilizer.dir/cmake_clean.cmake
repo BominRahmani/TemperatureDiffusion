311d366c868ff96609561d5792f473c047294c66
OzymandiasB <bominrahmani@gmail.com>
2022-02-17 18:39:13 -0500

    lol

---
+++ clonedStabilizer/cmake-build-debug/CMakeFiles/temperatureStabilizer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/temperatureStabilizer.dir/main.cpp.o"
+  "temperatureStabilizer"
+  "temperatureStabilizer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/temperatureStabilizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
